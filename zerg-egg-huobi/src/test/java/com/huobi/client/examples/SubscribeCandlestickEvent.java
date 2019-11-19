8c3e72160e79f800f69b028e5542760b4fb9590f
xuyang17 <xuyang17@guazi.com>
2019-11-19 14:00:09 +0800

    huobi-sdk

---
+++ zerg-egg-huobi/src/test/java/com/huobi/client/examples/SubscribeCandlestickEvent.java
@@ -0,0 +1,39 @@
+package com.huobi.client.examples;
+
+import com.huobi.client.SubscriptionClient;
+import com.huobi.client.model.enums.CandlestickInterval;
+
+import java.time.Instant;
+import java.util.Date;
+
+public class SubscribeCandlestickEvent {
+    public static void main(String[] args) {
+        SubscriptionClient subscriptionClient = SubscriptionClient.create();
+        String symbol = "btcusdt";
+//    subscriptionClient.subscribeCandlestickEvent(symbol, CandlestickInterval.MIN15, (candlestickEvent) -> {
+//      System.out.println("--------------- Subscribe Candlestick ------------------");
+//      System.out.println("id: " + candlestickEvent.getData().getId());
+//      System.out.println("Timestamp: " + candlestickEvent.getData().getTimestamp());
+//      System.out.println("High: " + candlestickEvent.getData().getHigh());
+//      System.out.println("Low: " + candlestickEvent.getData().getLow());
+//      System.out.println("Open: " + candlestickEvent.getData().getOpen());
+//      System.out.println("Close: " + candlestickEvent.getData().getClose());
+//      System.out.println("Volume: " + candlestickEvent.getData().getVolume());
+//    });
+
+        subscriptionClient.requestCandlestickEvent(symbol, 1325347200L, 2524579199L, CandlestickInterval.MIN15, (candlestickEvent) -> {
+            System.out.println("--------------- Request Candlestick ------------------");
+            System.out.println("===size:" + candlestickEvent.getData().size() + "===");
+            candlestickEvent.getData().forEach(candlestick -> {
+                System.out.println("id: " + new Date(candlestick.getId() * 1000));
+                System.out.println("Timestamp: " + candlestick.getTimestamp());
+                System.out.println("High: " + candlestick.getHigh());
+                System.out.println("Low: " + candlestick.getLow());
+                System.out.println("Open: " + candlestick.getOpen());
+                System.out.println("Close: " + candlestick.getClose());
+                System.out.println("Volume: " + candlestick.getVolume());
+                System.out.println("--------------------------------------------");
+            });
+        });
+    }
+}
---
+++ zerg-egg-huobi/src/test/java/com/huobi/client/examples/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.huobi.client.examples.constants;
+
+public class Constants {
+
+  public static final String API_KEY = "098be836-vfd5ghr532-86f96ad6-1ef8d";
+  public static final String SECRET_KEY = "0106cc24-47b23a0f-3e5365e2-e23f1";
+
+}
